f7906c4be3ebe56aad0eb5beaaa23f04e7af7be0
Satyam-R4j <satyamraj418@gmail.com>
2025-08-15 19:03:40 +0530

    Setuping project

---
+++ index.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+dotenv.config({
+    path: "./.env"
+})
+
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
+app.listen(port,()=>{
+    console.log(`App listening on port http://localhost:${port}`)
+})
