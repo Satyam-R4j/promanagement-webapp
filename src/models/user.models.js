80407895197beb241714758301e163883285e142
Satyam-R4j <satyamraj418@gmail.com>
2025-08-19 21:46:41 +0530

    Writing the Hashing functionality

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from 'mongoose'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import crypto from 'crypto'
 
 const schema = new Schema({
 
@@ -78,7 +79,7 @@ userSchema.methods.generateAccessToken = function ()
         _id: this._id,
         email: this.email,
         username: this.username
-    },  
+    },
         process.env.ACCESS_TOKEN_SECRET,
         {
             expiresIn: process.env.ACCESS_TOKEN_EXPIRY
@@ -87,9 +88,29 @@ userSchema.methods.generateAccessToken = function ()
     )
 }
 
-userSchema.methods.generateRefreshToken = function()
+userSchema.methods.generateRefreshToken = function ()
 {
-    
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    )
+}
+
+userSchema.methods.generateTemporaryToken = function ()
+{
+    const unHashedToken = crypto.randomBytes(20).toString("hex")
+
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(unHashedToken)
+        .digest("hex")
+
+    const tokenExpiry = Date.now() + (20*60*1000)
+    return {unHashedToken,hashedToken,tokenExpiry}
 }
 
 export const User = mongoose.model("User", userSchema)
