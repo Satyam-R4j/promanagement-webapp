40c8fc97d558a120100b7501a335bef0e5fe8e97
Satyam-R4j <satyamraj418@gmail.com>
2025-08-19 20:43:18 +0530

    Setup JWT

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const schema = new Schema({
 
@@ -54,8 +56,40 @@ const schema = new Schema({
     emailVerificationExpiry: {
         type: Date
     }
-},{
+}, {
     timestamps: true
 })
+
+userSchema.pre("save", async function (next)
+{
+    if (!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password)
+{
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function ()
+{
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username
+    },  
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+    )
+}
+
+userSchema.methods.generateRefreshToken = function()
+{
+    
+}
 
 export const User = mongoose.model("User", userSchema)
