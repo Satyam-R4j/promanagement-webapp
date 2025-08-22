862dbe5a3797f41c1a52f8c109dfae7c7c3a9880
Satyam-R4j <satyamraj418@gmail.com>
2025-08-22 14:45:21 +0530

    setup cookie generation and login functionality

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,77 +1,76 @@
-import { User } from '../models/user.models.js'
-import { ApiResponse } from "../utils/api-response.js"
-import { ApiError } from "../utils/api-error.js"
-import { asyncHandler } from '../utils/async-handler.js'
-import { emailVerificationGenContent, sendEmail } from '../utils/mail.js'
+import { User } from "../models/user.models.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { emailVerificationGenContent, sendEmail } from "../utils/mail.js";
 
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
 
-const generateAccessAndRefreshTokens = async (userId) =>
-{
-    try
-    {
-        const user = await User.findById(userId)
-        const accessToken = user.generateAccessToken()
-        const refreshToken = user.generateRefreshToken()
-
-        user.refreshToken = refreshToken
-        await user.save({ validateBeforeSave: false })
-        return { accessToken, refreshToken }
-    } catch (error)
-    {
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+        return { accessToken, refreshToken };
+    } catch (error) {
         throw new ApiError(
             500,
             "Something went wrong while generating access token"
-        )
+        );
     }
-}
+};
 
-
-
-
-
-const registerUser = asyncHandler(async (req, res) =>
-{
-    const { email, username, password, role } = req.body
+const registerUser = asyncHandler(async (req, res) => {
+    const { email, username, password, role } = req.body;
 
     const existedUser = await User.findOne({
-        $or: [{ username }, { email }]
-    })
+        $or: [{ username }, { email }],
+    });
 
-    if (existedUser)
-    {
-        throw new ApiError(409, "User with email or username already exits", [])
+    if (existedUser) {
+        throw new ApiError(
+            409,
+            "User with email or username already exits",
+            []
+        );
     }
 
     const user = await User.create({
         email,
         password,
         username,
-        isEmailVerified: false
-    })
+        isEmailVerified: false,
+    });
 
-    const { unHashedToken, hashedToken, tokenExpiry } = user.generateTemporaryToken()
+    const { unHashedToken, hashedToken, tokenExpiry } =
+        user.generateTemporaryToken();
 
-    user.emailVerificationToken = hashedToken
-    user.emailVerificationExpiry = tokenExpiry
+    user.emailVerificationToken = hashedToken;
+    user.emailVerificationExpiry = tokenExpiry;
 
-    await user.save({ validateBeforeSave: false })
+    await user.save({ validateBeforeSave: false });
 
     await sendEmail({
         email: user?.email,
         subject: "Please verify your email",
         mailGenContent: emailVerificationGenContent(
             user.username,
-            `${req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unHashedToken}`
-        )
-    })
+            `${req.protocol}://${req.get(
+                "host"
+            )}/api/v1/users/verify-email/${unHashedToken}`
+        ),
+    });
 
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken -emailVerificationToken -emailVerificationExpiry"
-    )
+    );
 
-    if (!createdUser)
-    {
-        throw new ApiError(500, "Something went wrong while registering a user")
+    if (!createdUser) {
+        throw new ApiError(
+            500,
+            "Something went wrong while registering a user"
+        );
     }
 
     return res
@@ -82,13 +81,51 @@ const registerUser = asyncHandler(async (req, res) =>
                 { user: createdUser },
                 "User registered successfully and verification email has been send to your mail"
             )
+        );
+});
+
+const login = asyncHandler(async (req, res) => {
+    const { email, password, username } = res.body;
+    if (!email) {
+        throw new ApiError(400, "Username or email is required");
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new ApiError(400, "User does not exist");
+    }
+
+    const isPasswordValid = user.isPasswordCorrect(password);
+    if (!isPasswordValid) {
+        throw new ApiError(400, "Invalid credentials");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+        user._id
+    );
+
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken -emailVerificationToken -emailVerificationExpiry"
+    );
+    const options = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+          new ApiResponse(
+            200,
+            {
+              user: loggedInUser,
+              accessToken,
+              refreshToken
+            },
+            "User logged in successfully"
+          )
         )
-})
+});
 
-
-export { registerUser }
-
-
-
-
-
+export { registerUser };
---
+++ src/validators/index.js
@@ -0,0 +1,24 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+    return [
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Email is invalid"),
+        body("username")
+            .trim()
+            .notEmpty()
+            .withMessage("User is required")
+            .isLowercase()
+            .withMessage("User name must be in lowercase")
+            .isLength({ min: 3 })
+            .withMessage("User name must be 3 characters long"),
+        body("password").trim().notEmpty().withMessage("Password is required"),
+        body("FullName").optional().trim(),
+    ];
+};
+
+export { userRegisterValidator };
