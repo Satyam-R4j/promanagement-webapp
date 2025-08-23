365639db15ef0c40ebeb83e01647b655e3958250
Satyam-R4j <satyamraj418@gmail.com>
2025-08-24 02:45:46 +0530

    implemented logout functionality

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -151,4 +151,44 @@ const logoutUser = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, {}, "User logged"));
 });
 
-export { registerUser, login, logoutUser };
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, req.user, "Current User fetched successfully")
+        );
+});
+
+const verifyEmail = asyncHandler(async (req, res) => {
+    const { verificationToken } = req.params;
+    if (!verificationToken) {
+        throw new ApiError(400, "Email verification token is missing");
+    }
+    let hashedToken = crypto
+        .createHash("sha256")
+        .update(verificationToken)
+        .digest("hex");
+
+    const user = await User.findOne({
+        emailVerificationToken: hashedToken,
+        emailVerificationExpiry: { $gt: Data.now() },
+    });
+
+    if (!user) {
+        throw new ApiError(400, "Token is invalid or expired");
+    }
+
+    user.emailVerificationToken = undefined;
+    user.emailVerificationExpiry = undefined;
+
+    user.isEmailVerified = true;
+    await user.save({ validateBeforeSave: false });
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, { isEmailVerified: true }, "Email is verified")
+        );
+});
+
+export { registerUser, login, logoutUser, getCurrentUser, verifyEmail };
