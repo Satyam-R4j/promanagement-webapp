38ec28216bd777ffc4dc8ed4b354ae45c1c93abc
Satyam-R4j <satyamraj418@gmail.com>
2025-08-23 21:48:47 +0530

    Working on logout middleware

--- src/Routes/auth.routes.js
+++ src/Routes/auth.routes.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
-import { registerUser , login } from "../controllers/auth.controllers.js";
+import {
+    registerUser,
+    login,
+    logoutUser,
+} from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
-import { userRegisterValidator } from "../validators/index.js";
+import {
+    userLoginValidator,
+    userRegisterValidator,
+} from "../validators/index.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 
-router.route("/login").post(userRegisterValidator(), validate, login);
+router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
